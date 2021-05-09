3ab9ebe4cb42d31063637b14a29da0b5a3d03d97
random <283739569@qq.com>
2021-05-09 17:06:31 +0800

    1

---
+++ BIJKPlayer/IJKMediaPlayer/IJKMediaPlayer/ijkmedia/ijksdl/ios/IJKSDLHudViewCell.h
@@ -0,0 +1,20 @@
+//
+//  IJKSDLHudViewCell.h
+//  IJKMediaPlayer
+//
+//  Created by Zhang Rui on 15/12/14.
+//  Copyright © 2015年 bilibili. All rights reserved.
+//
+
+#ifndef IJK_OSX
+#import <UIKit/UIKit.h>
+
+@interface IJKSDLHudViewCell : UITableViewCell
+
+- (id)init;
+
+- (void)setHudValue:(NSString *)value forKey:(NSString *)key;
+
+@end
+
+#endif
---
+++ BIJKPlayer/IJKMediaPlayer/IJKMediaPlayer/ijkmedia/ijksdl/ios/IJKSDLHudViewController.h
@@ -0,0 +1,20 @@
+//
+//  IJKSDLHudViewController.h
+//  IJKMediaPlayer
+//
+//  Created by Zhang Rui on 15/12/14.
+//  Copyright © 2015年 bilibili. All rights reserved.
+//
+
+#ifndef IJK_OSX
+#import <UIKit/UIKit.h>
+
+@interface IJKSDLHudViewController : UITableViewController
+
+- (id)init;
+
+- (void)setHudValue:(NSString *)value forKey:(NSString *)key;
+
+- (void)setRect:(CGRect) rect;
+@end
+#endif
